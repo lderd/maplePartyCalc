e42ff0f62147180114214c7ed75f017a775a15ee
unknown <dkshktpq@naver.com>
2024-02-07 19:16:30 +0900

    gray scale적용, 임계처리로 배경 삭제

---
+++ test.py
@@ -0,0 +1,37 @@
+from PIL import Image
+from pytesseract import *
+import cv2
+
+
+filename = "./sample.png"
+# filename1 = "./no_noise.png"
+
+# 이미지 전처리
+# img = cv2.imread(filename)
+# grayscale 이미지 생성
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+cv2.imshow('원본', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+ret, thresh1 = cv2.threshold(img, 127,255, cv2.THRESH_BINARY)
+cv2.imshow('임계처리', thresh1)
+
+# 키 입력 전까지 화면에 이미지 띄우기
+cv2.waitKey(0)
+
+# 이미지 파일 생성
+cv2.imwrite('greyScaleSample.png', thresh1)
+
+
+# 화면에 뜬 이미지 창 닫기
+cv2.destroyAllWindows()
+
+# img to txt file
+image = Image.open('./greyScaleSample.png')
+
+text = image_to_string(image, lang="kor")
+
+with open("after_sample.txt", "w") as f:
+    f.write(text)
+# img to txt file
