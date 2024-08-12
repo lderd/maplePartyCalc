912b46d9c5528c99e18f66d96ecfe44ebd5aa71c
unknown <dkshktpq@naver.com>
2024-08-12 17:49:19 +0900

    .env 파일 위치 수정

--- app/main.py
+++ app/main.py
@@ -11,12 +11,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
+    dirname: str = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
 
     apilayer_api_key: str = Field("", alias="APILAYER_KEY")
     aws_access_key: str = ""
     aws_secret_access_key: str = ""
     bucket_name: str = Field("", alias="AWS_BUCKET_NAME")
-    dirname: str = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
 
 
 settings = Settings()
@@ -24,9 +24,9 @@ settings = Settings()
 app = FastAPI()
 
 
-@app.get("/calc", response_class=FileResponse)
+@app.get("/calc")
 def calc():
-    return FileResponse(f"{settings.dirname}/static/calc.html")
+    return{"settings.apilayer_api_key":settings.apilayer_api_key, "settings.aws_access_key":settings.aws_access_key, "settings.aws_secret_access_key":settings.aws_secret_access_key, "settings.bucket_name":settings.bucket_name, "settings.dirname":settings.dirname}
 
 @app.get("/word", response_class=FileResponse)
 def word():
@@ -42,28 +42,24 @@ def ImgToText():
 
 @app.post("/imgToText")
 async def ImgToTextPost(file: UploadFile):
-    try :
-        img = Image.open(file.file)
-        filename = secrets.token_urlsafe(16) + ".png"
-        img.save(f'{settings.dirname}/images/{filename}', "png")
-        # s3 작업
-        s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=settings.aws_access_key, # 본인 소유의 키를 입력
-        aws_secret_access_key=settings.aws_secret_access_key, # 본인 소유의 키를 입력
-        region_name="us-east-2",
-        )
-        s3_client.upload_file(f'{settings.dirname}/images/{filename}', settings.bucket_name, filename)
-        # s3 작업
-
-        # img to text
-        url = f"https://api.apilayer.com/image_to_text/url?url=https://myimagetotextproject.s3.us-east-2.amazonaws.com/{filename}"
-        payload = {}
-        headers= {"apikey": settings.apilayer_api_key}
-        response = requests.request("GET", url, headers=headers, data = payload).json()
-        
-        os.remove(f'{settings.dirname}/images/{filename}')
-    except :
-        return {"all_text": "문제가 발생했습니다 다시 시도해 주세요"}
+    img = Image.open(file.file)
+    filename = secrets.token_urlsafe(16) + ".png"
+    img.save(f'{settings.dirname}/images/{filename}', "png")
+    # s3 작업
+    s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=settings.aws_access_key, # 본인 소유의 키를 입력
+    aws_secret_access_key=settings.aws_secret_access_key, # 본인 소유의 키를 입력
+    region_name="ap-northeast-2",
+    )
+    s3_client.upload_file(f'{settings.dirname}/images/{filename}', settings.bucket_name, filename)
+    # s3 작업
+    # img to text
+    url = f"https://api.apilayer.com/image_to_text/url?url=https://mapleitemlogbucket.s3.ap-northeast-2.amazonaws.com/{filename}"
+    payload = {}
+    headers= {"apikey": settings.apilayer_api_key}
+    response = requests.request("GET", url, headers=headers, data = payload).json()
+    
+    os.remove(f'{settings.dirname}/images/{filename}')
 
     return response
